5853a9299ddf705f6fffbc3494af6cb1604a2903
Konark-2ez <konarkkumar456@gmail.com>
2023-01-20 15:48:51 +0530

    added day-4

--- index.js
+++ index.js
@@ -157,6 +157,40 @@ function showSlidesOne(n){
   slides1[slideIndexOne-1].getElementsByClassName.display = "block"
 }
 
+//CAROUSEL-3
+let arr=[
+  "https://www.reliancedigital.in/medias/Redmi-Note-12-5G-Series-NPI-Banner-11-01-2023.jpg?context=bWFzdGVyfGltYWdlc3w2MTM2M3xpbWFnZS9qcGVnfGltYWdlcy9oNGMvaDZmLzk5NDQ5MDc0NDgzNTAuanBnfDY3MGJlYzc4OTFkYzE4Y2NlYTFhMTRjOTY0MjNkZGJlNzNmYzQ4MTkxYTkxODcyZjczZTllMTlmMjc2OGZkZDk",
+  "https://www.reliancedigital.in/medias/airpods-pro-2nd-gen-Buy-now-NPI-Banner-01-11-2022.jpg?context=bWFzdGVyfGltYWdlc3wxMDM4NjB8aW1hZ2UvanBlZ3xpbWFnZXMvaDQ2L2g2Ny85OTIyNDU2MTkxMDA2LmpwZ3w0MGQxZjFhYjQ0ZjU1NTQzZjJiOGJmYzZhMDg5NTY5M2Y3NzIxZDkzOTQ2Yjg4YmNmOWZlMzc4OWM0YjlkMjA2",
+  "https://www.reliancedigital.in/medias/Bose-Earbuds-NPI-Banner-19-01-2023.jpg?context=bWFzdGVyfGltYWdlc3w1NjI4OHxpbWFnZS9qcGVnfGltYWdlcy9oNWUvaDIzLzk5NDk5NjEzODgwNjIuanBnfDU5OTYxYmIzNjI3NDRkOGI0YzMwMGM4NmM2OGU0OTZmODRhMWRjYmMzM2ExMjRiMzRkOTM4M2Y4OGJlNmY4ZTc"
+]
+let prevB = document.querySelector(".prev")
+var currentImageIndex1 = 0;
+var currentImage1 = document.getElementById("current_image3");
+
+let nexB = document.querySelector(".next")
+
+previous.addEventListener("click",()=>{
+  currentImageIndex1 = (currentImageIndex1 +arr.length - 1) % arr.length
+ 
+  displayImage()
+})
+nex.addEventListener("click",()=>{
+  currentImageIndex1 = (currentImageIndex1 +arr.length + 1) % arr.length
+  displayImage()
+})
+
+displayImage()
+function displayImage(){
+  currentImage1.setAttribute("src",arr[currentImageIndex1])
+ // console.log(currentImage1)
+}
+setInterval(() => {
+  
+  currentImageIndex1 = (currentImageIndex1 + 1) % arr.length
+  displayImage()
+}, 2000);
+
+//FOR SEARCH
 
 
 
